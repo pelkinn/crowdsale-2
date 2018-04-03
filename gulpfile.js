2abe4fb584bb94bbbc9d3405f110b27a2d361418
pelkinn <mrpelkin@gmail.com>
2018-04-03 13:30:47 +0300

    add button JoinUs/ gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    plumber = require("gulp-plumber"),
+    autoprefixer = require('gulp-autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rename = require("gulp-rename");
+
+gulp.task("style", function() {
+    gulp.src("scss/main.scss")
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest("css"))
+        .pipe(rename("style.css"))
+
+        .pipe(gulp.dest("css"))
+});
+
+
+gulp.watch('scss/**/*.scss', ['style']);
